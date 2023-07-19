da943781e852e36405be834ad2ff033b5c253d21
Clanie1 <danybarociov@gmail.com>
2023-07-19 12:48:36 -0700

    SideBar logos and links

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,20 +1,26 @@
-import { CiFacebook } from "react-icons/ci";
+import { BsGithub } from "react-icons/bs";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { BiLogoGmail, BiLogoLinkedin } from "react-icons/bi";
 
 const SideBar = () => {
   return (
-    <div className="border-r-[0.5px] px-4 py-4 flex flex-col gap-10 border-[#ffffff22]">
-      <a href="#">
-        <CiFacebook style={{ width: "30px", height: "30px" }} />
-      </a>
-      <a href="#">
-        <CiFacebook style={{ width: "30px", height: "30px" }} />
-      </a>
-      <a href="#">
-        <CiFacebook style={{ width: "30px", height: "30px" }} />
-      </a>
-      <a href="#">
-        <CiFacebook style={{ width: "30px", height: "30px" }} />
-      </a>
+    <div className="border-r-[0.5px] px-4 py-4 border-[#ffffff22] max-w-lg">
+      <div className="flex flex-col gap-20 border-[#ffffff22] max-w-lg mt-10">
+        <a href="https://github.com/Clanie1">
+          <BsGithub className="w-[30px] h-[30px] hover:scale-110 duration-75" />
+        </a>
+
+        <a href="https://www.linkedin.com/in/daniel-barocio-2746981a0/">
+          <BiLogoLinkedin className="w-[30px] h-[30px] hover:scale-110 duration-75" />
+        </a>
+
+        <a href="https://www.instagram.com/danibarocio/">
+          <AiOutlineInstagram className="w-[30px] h-[30px] hover:scale-110 duration-75" />
+        </a>
+        <a href="#">
+          <BiLogoGmail className="w-[30px] h-[30px] hover:scale-110 duration-75" />
+        </a>
+      </div>
     </div>
   );
 };
