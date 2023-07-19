c1e9849906789b93d3dbac891aec4f3161a34797
Clanie1 <danybarociov@gmail.com>
2023-07-19 12:51:15 -0700

    Download Resume

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,4 +1,5 @@
 import { AiOutlineDownload } from "react-icons/ai";
+import ResumeDaniel from "../../assets/resume/Resume Daniel.pdf";
 
 const Navbar = () => {
   const NAVBAR_OPTIONS = ["Home", "Projects", "About-me", "Contact"];
@@ -23,14 +24,21 @@ type NavbarOptionProps = {
 
 const DownloadResume = () => {
   return (
-    <button className=" flex justify-center items-center  gap-[.3em] bg-white text-sm px-4 py-2 font-light rounded-[30px] text-black h-fit min-w-[200px] hover:bg-slate-200 duration-75">
-      Download Resume
-      <AiOutlineDownload style={{ width: "20px", height: "20px" }} />
-      <span className="absolute top-2 right-2 flex h-3 w-3">
-        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75" />
-        <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500" />
-      </span>
-    </button>
+    <a
+      href={ResumeDaniel}
+      download="DanielResume"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <button className=" flex justify-center items-center  gap-[.3em] bg-white text-sm px-4 py-2 font-light rounded-[30px] text-black h-fit min-w-[200px] hover:bg-slate-200 duration-75">
+        Download Resume
+        <AiOutlineDownload style={{ width: "20px", height: "20px" }} />
+        <span className="absolute top-2 right-2 flex h-3 w-3">
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75" />
+          <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500" />
+        </span>
+      </button>
+    </a>
   );
 };
 
