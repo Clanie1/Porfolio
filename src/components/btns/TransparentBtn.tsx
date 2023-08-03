31902b13409b806d9f4b5741bb4c5ace0d593303
Clanie1 <danybarociov@gmail.com>
2023-08-02 19:20:54 -0700

    hover on transparentbtn

--- src/components/btns/TransparentBtn.tsx
+++ src/components/btns/TransparentBtn.tsx
@@ -9,7 +9,7 @@ const TransparentBtn = ({
 }) => {
   return (
     <a href={href}>
-      <button className="border-[1px] border-white md:w-[200px] px-4 py-4 flex justify-center items-center">
+      <button className="border-[1px] border-white md:w-[200px] px-4 py-4 flex justify-center items-center hover:bg-slate-800 duration-75">
         {children}
       </button>
     </a>
